53b2908679bb95ca6d8b6826bac7c64562f97c12
rithik <115656594+theRithik@users.noreply.github.com>
2023-04-09 21:59:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,22 +10,33 @@ app.get('/',(req,res)=>{
     res.send('this is from nodejs 8 class')
 })
 
-app.get('/hello',(req,response)=>{
+app.get('/list',(req,response)=>{
     const url = 'http://localhost:3000/movies'
-    // fetch(url,{method:'GET'})
-    // .then((req)=>{req.json()})
-    // .then((res)=>{ console.log(res)})
-    // .catch((err)=>{console.log(err)})
     axios.get(url)
         .then((res)=>{
         const result = res.data
-        for(let i=0; i<result.lenght; i++){
-            response.send(result[i].name)
+        const finalResult = []
+        for(let i=0; i<result.length; i++){
+            const time = new Date()
+            const release = new Date(result[i].date)            
+            const timediff = time.getTime() - release.getTime()
+            const diff = timediff/(1000*60*60*24)
+
+            if(diff>3){
+                result[i].status = 'expired'
+            }
+            else if(diff>=1){
+                result[i].status ='running'
+            }
+            else{
+                result[i].status ='just released'
+            }
+            finalResult.push(result[i])
         }
-        
+      
+            response.render('homepage.ejs',{movies:finalResult})
         })
-       
-    
+        .catch((err)=>{console.log(err)})
    
 })
 app.listen(4000,()=>{
